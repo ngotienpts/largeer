7c9e8d6c8e58abcc67ac063f5e9b2915c60e3247
NGOVANTIEN <ngotienpts@gmail.com>
2021-08-03 21:07:42 +0700

    3/8/2021

--- js/main.js
+++ js/main.js
@@ -10,3 +10,35 @@ closeSearchHeader.onclick = function() {
         showSearchHeader.classList.remove("open");
     }
 };
+// end show search
+
+// show notification
+var notifiHeader = document.querySelector(".header-notifi");
+var showNotifi = document.querySelector(".show-notifi");
+var hideBadge = document.querySelector(".header-notifi-badge");
+notifiHeader.onclick = function() {
+    if (showNotifi.classList.contains("open")) {
+        showNotifi.classList.remove("open");
+    } else {
+        showNotifi.classList.add("open");
+        hideBadge.style.display = "none";
+    }
+};
+// end show notification
+
+// show popup tao bai viet
+var createPost = document.querySelector(".navbar-index_right__title");
+var getPopup = document.querySelector(".create-post-wrapper");
+var closePost = document.querySelector(".create-post__close");
+createPost.onclick = function() {
+    if (getPopup.classList.contains("open")) {
+        getPopup.classList.remove("open");
+    } else {
+        getPopup.classList.add("open");
+    }
+};
+closePost.onclick = function() {
+    if (getPopup.classList.contains("open")) {
+        getPopup.classList.remove("open");
+    }
+};
