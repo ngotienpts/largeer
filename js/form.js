144798e429a8efe432655f92718cb0c2dfdd7e39
NGOVANTIEN <ngotienpts@gmail.com>
2021-08-30 21:09:40 +0700

    30/08/2021

---
+++ js/form.js
@@ -0,0 +1,205 @@
+// register-form
+function formRegister() {
+    let blockForm = document.querySelector("#form-register");
+
+    let phoneNumberElement = document.querySelector(
+        "input[type='tel'][name='phone']"
+    );
+
+    let passwordElement = document.querySelector(
+        'input[type="password"][name="password"]'
+    );
+
+    let showidIntro = document.querySelector(
+        'input[type="text"][name="id-intro"]'
+    );
+
+    let showEye = document.querySelector(".show-password");
+
+    let clickIconShowIntro = document.querySelector(".show-id-intro");
+
+    phoneNumberElement.onblur = function(e) {
+        let evenElement = e.target.value;
+        let filterPhone = /^\d{10}$/;
+        let formMessage = document.querySelector(".form-message");
+        if (evenElement.trim() == "" || !evenElement.match(filterPhone)) {
+            phoneNumberElement.parentElement.parentElement.classList.add("invalid");
+            formMessage.innerText = "Số điện thoại của bạn không hợp lệ";
+        } else {
+            phoneNumberElement.parentElement.parentElement.classList.remove(
+                "invalid"
+            );
+            formMessage.innerText = "";
+        }
+    };
+
+    phoneNumberElement.oninput = function(e) {
+        let formMessage = document.querySelector(".form-message");
+        if (e.value != "") {
+            phoneNumberElement.parentElement.parentElement.classList.remove(
+                "invalid"
+            );
+            formMessage.innerText = "";
+        }
+    };
+
+    // show password
+    showEye.onclick = function(e) {
+        e.preventDefault();
+        if (
+            passwordElement.getAttribute("type") === "password" &&
+            showEye.getAttribute("name") === "eye-off-outline"
+        ) {
+            passwordElement.setAttribute("type", "text");
+            showEye.setAttribute("name", "eye-outline");
+        } else {
+            passwordElement.setAttribute("type", "password");
+            showEye.setAttribute("name", "eye-off-outline");
+        }
+    };
+
+    //password
+    passwordElement.onblur = function(e) {
+        let evenElement = e.target.value;
+        let filterPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,13}$/;
+        let formMessage =
+            passwordElement.parentElement.parentElement.querySelector(
+                ".form-message"
+            );
+        if (evenElement.trim() == "" || !evenElement.match(filterPassword)) {
+            passwordElement.parentElement.parentElement.classList.add("invalid");
+            formMessage.innerText =
+                "Mật khẩu phải có từ 8-20 ký tự gồm chữ và số in hoa";
+        } else {
+            passwordElement.parentElement.parentElement.classList.remove("invalid");
+            formMessage.innerText = "";
+        }
+    };
+
+    passwordElement.oninput = function(e) {
+        if (e.target.value != "") {
+            let formMessage =
+                passwordElement.parentElement.parentElement.querySelector(
+                    ".form-message"
+                );
+            passwordElement.parentElement.parentElement.classList.remove("invalid");
+            formMessage.innerText = "";
+        }
+    };
+
+    // show id intro
+    clickIconShowIntro.onclick = function() {
+        if (showidIntro.classList.contains("hide")) {
+            showidIntro.classList.remove("hide");
+            showidIntro.classList.add("show");
+        } else {
+            showidIntro.classList.add("hide");
+            showidIntro.classList.remove("show");
+        }
+    };
+}
+// login form
+function formLogin() {
+    let blockFormLogin = document.querySelector("#form-login");
+
+    let phoneNumberElementLogin = blockFormLogin.querySelector(
+        "input[type='tel'][name='phone']"
+    );
+
+    let passwordElement = document.querySelector(
+        'input[type="password"][name="password"]'
+    );
+
+    let showEye = document.querySelector(".show-password");
+
+    phoneNumberElementLogin.onblur = function(e) {
+        let evenElement = e.target.value;
+        let filterPhone = /^\d{10}$/;
+        let formMessage = blockFormLogin.querySelector(".form-message");
+        if (evenElement.trim() == "" || !evenElement.match(filterPhone)) {
+            phoneNumberElementLogin.parentElement.parentElement.classList.add(
+                "invalid"
+            );
+            formMessage.innerText = "Số điện thoại của bạn không hợp lệ";
+        } else {
+            phoneNumberElementLogin.parentElement.parentElement.classList.remove(
+                "invalid"
+            );
+            formMessage.innerText = "";
+        }
+    };
+
+    phoneNumberElementLogin.oninput = function(e) {
+        let formMessage = blockFormLogin.querySelector(".form-message");
+        if (e.value != "") {
+            phoneNumberElementLogin.parentElement.parentElement.classList.remove(
+                "invalid"
+            );
+            formMessage.innerText = "";
+        }
+    };
+
+    // show password
+    showEye.onclick = function(e) {
+        e.preventDefault();
+        if (
+            passwordElement.getAttribute("type") === "password" &&
+            showEye.getAttribute("name") === "eye-off-outline"
+        ) {
+            passwordElement.setAttribute("type", "text");
+            showEye.setAttribute("name", "eye-outline");
+        } else {
+            passwordElement.setAttribute("type", "password");
+            showEye.setAttribute("name", "eye-off-outline");
+        }
+    };
+
+    //password
+    passwordElement.onblur = function(e) {
+        let evenElement = e.target.value;
+        let filterPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
+        let formMessage =
+            passwordElement.parentElement.parentElement.querySelector(
+                ".form-message"
+            );
+        if (evenElement.trim() == "" || !evenElement.match(filterPassword)) {
+            passwordElement.parentElement.parentElement.classList.add("invalid");
+            formMessage.innerText =
+                "Mật khẩu phải có từ 8-20 ký tự gồm chữ và số in hoa";
+        } else {
+            passwordElement.parentElement.parentElement.classList.remove("invalid");
+            formMessage.innerText = "";
+        }
+    };
+
+    passwordElement.oninput = function(e) {
+        if (e.target.value != "") {
+            let formMessage =
+                passwordElement.parentElement.parentElement.querySelector(
+                    ".form-message"
+                );
+            passwordElement.parentElement.parentElement.classList.remove("invalid");
+            formMessage.innerText = "";
+        }
+    };
+}
+
+// otp
+function otpForm() {
+    var nextElement = 0;
+    var blockOtp = document.querySelectorAll(".otSc");
+    var output = blockOtp.forEach(function(input, index) {
+        input.oninput = function(e) {
+            e.target.value = e.target.value.replace(/[^0-9]/g, "");
+        };
+        input.onkeyup = function(e) {
+            this.nextElement++;
+            if (e.target.value != "") {
+                var nextEl = this.nextElementSibling;
+                nextEl.focus();
+                console.log(blockOtp.length);
+                console.log(this.nextElement++);
+            }
+        };
+    });
+}
--- js/main.js
+++ js/main.js
@@ -54,22 +54,6 @@ document.onkeydown = function(e) {
     }
 };
 // end show popup tao cong dong
-// show form login
-var showLoginForm = document.querySelector(".user-login");
-var getPopupLogin = document.querySelector(".login-wrapper");
-var closePopLogin = document.querySelector(".login-btn-close");
-showLoginForm.onclick = function() {
-    if (getPopupLogin.classList.contains("open")) {
-        getPopupLogin.classList.remove("open");
-    } else {
-        getPopupLogin.classList.add("open");
-    }
-};
-closePopLogin.onclick = function() {
-    if (getPopupLogin.classList.contains("open")) {
-        getPopupLogin.classList.remove("open");
-    }
-};
 // show notification
 var notifiHeader = document.querySelector(".header-notifi");
 var showNotifi = document.querySelector(".show-notifi");
@@ -101,85 +85,3 @@ closewallet.onclick = function() {
     }
 };
 // end show popup vi cua toi
-// end show form login
-// upload 1 file
-// function readURL(input, imgControlName) {
-//     if (input.files && input.files[0]) {
-//         var reader = new FileReader();
-//         reader.onload = function(e) {
-//             $(imgControlName).attr("src", e.target.result);
-//         };
-//         reader.readAsDataURL(input.files[0]);
-//     }
-// }
-// $("#imag").change(function() {
-//     var imgControlName = "#ImgPreview";
-//     readURL(this, imgControlName);
-//     $(".preview1").addClass("it");
-//     $(".btn-rmv1").addClass("rmv");
-// });
-// $("#removeImage1").click(function(e) {
-//     e.preventDefault();
-//     $("#imag").val("");
-//     $("#ImgPreview").attr("src", "");
-//     $(".preview1").removeClass("it");
-//     $(".btn-rmv1").removeClass("rmv");
-// });
-// upload nhieu file
-var upload = $(".imag")[0],
-    infos = $("#block-preview");
-
-upload.onchange = function() {
-    readFilesInfo(this.files, 0);
-};
-
-function readFilesInfo(files, i) {
-    var item = "",
-        i = i || 0,
-        filesNum = files.length,
-        reader = null;
-
-    reader = readAsDataURL(files[i]);
-    reader.onloadend = function() {
-        item =
-            '<div class="article">' +
-            (isImage(files[i]) ?
-                '<img src="' +
-                reader.result +
-                '" width="auto" height="90px"><input type="button" id="removeImage1" value="x" class="rmv" />' :
-                "") +
-            "</div>";
-
-        i++;
-        infos.innerHTML = infos.innerHTML + item;
-
-        if (i < filesNum) {
-            readFilesInfo(files, i);
-        }
-    };
-}
-
-function readAsDataURL(file) {
-    var reader = new FileReader();
-    reader.readAsDataURL(file);
-    return reader;
-}
-
-function isImage(file) {
-    return /image/.test(file.type);
-}
-
-function $(selector, oParent) {
-    if (typeof selector === "object") {
-        return selector;
-    }
-    oParent = oParent || document;
-    switch (selector.charAt(0)) {
-        case "#":
-            return oParent.querySelector(selector);
-        default:
-            return oParent.querySelectorAll(selector);
-    }
-}
-// upload nhieu file
-// end upload file
